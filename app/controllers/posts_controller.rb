78cf6b8bdad7222d8bc5199076375696980a6b00
woshityj <yujietan84@gmail.com>
2021-05-07 22:54:20 +0800

    Ability to Create Posts and Sort Posts according to their created timing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,19 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def create
-    
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render root_path
+    end
+  end
+
+  private
+  def post_params
+    params.permit(:post, :user_id)
   end
 end
