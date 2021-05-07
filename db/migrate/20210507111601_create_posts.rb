e242354888a8c66de3aa556f7092fdd89e50a23d
woshityj <yujietan84@gmail.com>
2021-05-07 22:05:01 +0800

    Able to view Posts Dynamically

--- db/migrate/20210507111601_create_posts.rb
+++ db/migrate/20210507111601_create_posts.rb
@@ -2,7 +2,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :post
-      t.references :user_id, index: true, foreign_key: {to_table: :users}
 
       t.timestamps
     end
--- db/migrate/20210507115252_create_likes.rb
+++ db/migrate/20210507115252_create_likes.rb
@@ -2,7 +2,6 @@ class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
       t.integer :likes
-      t.references :post, index: true, foreign_key: {to_table: :posts}
 
       t.timestamps
     end
